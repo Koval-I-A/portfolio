56ac4609a91c4a4610dcf0022ab1922df3767b3f
Igor Koval <Koval_I_A@i.ua>
2014-10-28 13:37:27 +0200

    -v 0.1

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@ var gulp = require('gulp'),
 	connect = require('gulp-connect'),
 	rename = require('gulp-rename'),
 	uglify = require('gulp-uglify'),
-	csso = require('gulp-csso');
+	csso = require('gulp-csso'),
+    less = require('gulp-less'),
+    path = require('path');
 
 gulp.task('connect', function() {
   connect.server({
@@ -13,13 +15,13 @@ gulp.task('connect', function() {
   });
 });
 
-gulp.task('css', function() {
-    gulp.src('./css/dev/style.css')
-        .pipe(rename('style.min.css'))
-        .pipe(csso())
-        .pipe(gulp.dest('./css'))
-        .pipe(connect.reload());
-});
+// gulp.task('css', function() {
+//     gulp.src('./css/dev/style.css')
+//         .pipe(rename('style.min.css'))
+//         .pipe(csso())
+//         .pipe(gulp.dest('./css'))
+//         .pipe(connect.reload());
+// });
 
 gulp.task('js', function() {
     gulp.src('./js/dev/app.js')
@@ -30,14 +32,25 @@ gulp.task('js', function() {
 });
 
 gulp.task('watch', function() {
-    gulp.start('css');
+    gulp.start('less');
     gulp.start('js');
     gulp.watch('./js/dev/app.js', function() {
         gulp.start('js');
     });
-    gulp.watch('./css/dev/style.css', function() {
-        gulp.start('css');
+    gulp.watch('./css/dev/style.less', function() {
+        gulp.start('less');
     });
 });
 
-gulp.task('default', ['css', 'js', 'connect', 'watch']);
+gulp.task('less', function () {
+  gulp.src('./css/dev/style.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
+    .pipe(rename('style.min.css'))
+    .pipe(csso())
+    .pipe(gulp.dest('./css'))
+    .pipe(connect.reload());
+});
+
+gulp.task('default', ['less', 'js', 'connect', 'watch']);
